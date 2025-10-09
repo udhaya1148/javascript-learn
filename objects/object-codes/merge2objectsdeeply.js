9765a155f6c48af2fc2454d8eebc22cec6b722d3
udhaya1148 <udhayauk1148@gmail.com>
2025-10-09 17:42:10 +0530

    learn preactice-3

--- objects/object-codes/merge2objectsdeeply.js
+++ objects/object-codes/merge2objectsdeeply.js
@@ -20,3 +20,5 @@ let newDetails = {...companyDetails};
  console.log('After update',newDetails)  // while print this using spread operator company name is primitive type so it do deep copy ,
  //  employes is refernce type it keep aray so it do shallow copy
 
+//here deep copy stores primitive data types in objects each primitive data types values store in new independent memory it doesnot refer any values
+//shallow copy stores reference types(object,arrays) in object each values refer the same array/object. If one array changes all others also changed
